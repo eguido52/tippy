8cc7643817b0785f28708f02f826cfeee809e8bf
Emmanuel Guido <guid6241@m506-118-26457.local>
2019-01-23 15:02:59 -0800

    Initial Commit

---
+++ tippy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tippy
+//
+//  Created by Emmanuel Guido on 1/23/19.
+//  Copyright © 2019 Emmanuel Guido. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
